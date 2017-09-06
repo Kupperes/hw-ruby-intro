d38066f22d6c947573c9f1be000b68ab4031d7bf
kupperes <kupperes@gmail.com>
2017-09-06 01:39:46 +0000

    HW complete and commented

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -12,10 +12,10 @@ end
 
 def max_2_sum arr
   total = 0 
-  if arr.sort.reverse.first(2).reduce(:+) == nil
-    return 0
+  if arr.sort.reverse.first(2).reduce(:+) == nil  #see if array is empty
+    return 0                                      #if empty return 0
   else
-  total += arr.sort.reverse.first(2).reduce(:+)
+  total += arr.sort.reverse.first(2).reduce(:+)   #reverse sort and add two largest
   total
   end
 end
@@ -23,10 +23,9 @@ end
 
 def sum_to_n? arr, n
   arr.each do |x|
-    arr.each do |y|    #ISSUE: returns true when any two elements sum to the second argument [30 points] (FAILED - 1)
-      if arr[x] == arr[y]
-        #skip this iteration
-      elsif (x + y) == n
+    arr.each do |y|   
+      if arr[x] == arr[y]                       #skip this iteration because it is the same number
+      elsif (x + y) == n                        #add two numbers and check against n
         return true
       end
     end
@@ -41,28 +40,24 @@ def hello name
 end
 
 def starts_with_consonant? s 
-  first_letter = s[0]
-  if first_letter == nil
+  first_letter = s[0]                   #initialize first letter variable
+  if first_letter == nil                #check if string is empty
     return false
   end
-  "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ".include? first_letter
+  "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ".include? first_letter  #check if consonant
 end
 
 def binary_multiple_of_4? s   
-  s.each_char do |x|
+  s.each_char do |x|                     #check if valid binary
     if x != "1" && x != "0"
       return false
     end
   end
-  return true if s.length == 1 && s == '0'
-  if s.length > 2 && s.end_with?("00")  #"Incorrect results for input: \"#{string}\""
-                                        #Incorrect results for input: "1010101010100"
-    return true
-  end                      
+  return true if s.length == 1 && s == '0'     #test if multiple of 4
+  return true if s.length > 2 && s.end_with?("00")
+                                      
   return false
 end
-
-                                            #return s =~  /^[10]*00$/i
 
 # Part 3
 
@@ -70,30 +65,30 @@ class BookInStock
 
 attr_accessor :isbn, :price
 
-def initialize (book_str, price_str)
+def initialize (book_str, price_str)          #initialize instance and throw errors
   raise ArgumentError.new("Not valid ISBN") if book_str.length == 0
   @isbn = book_str
   @price = (price_str.to_f * 100).round / 100.00
   raise ArgumentError.new("Not valid price") if price <= 0
 end
 
-def isbn= (book_str)
+def isbn= (book_str)                          #isbn setter
   @isbn = book_str
 end
 
-def price= (price_str)
+def price= (price_str)                        #price setter to float
   @price = (price_str.to_f * 100).round / 100.00
 end
 
-def isbn
+def isbn                                      #isbn getter
   @isbn
 end
 
-def price
+def price                                     #price getter as float
   @price
 end
 
-def price_as_string
+def price_as_string                           #price getter as string
   sprintf"$%.2f",@price
 end
 
